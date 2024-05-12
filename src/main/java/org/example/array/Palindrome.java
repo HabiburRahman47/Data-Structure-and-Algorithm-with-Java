dbb876ab6f947e96556f70ecc9c7b6ffa80dcfa7
Habibur Rahman <habiburkuetacbd@gmail.com>
2024-05-12 19:48:20 +0600

    solution string palindrome

---
+++ src/main/java/org/example/array/Palindrome.java
@@ -0,0 +1,38 @@
+package org.example.array;
+
+public class Palindrome {
+
+    // method 1
+    public static boolean isPalindrome(String input){
+        boolean palindrome = true;
+        int n = input.length();
+        for (int i=0;i<n;i++){
+            if(input.charAt(i) != input.charAt(n-1-i)){
+                palindrome = false;
+            }
+        }
+        return palindrome;
+    }
+
+    // method 2
+    public static boolean isPalindrome2(String word){
+        char[] charArray = word.toCharArray();
+        int start = 0;
+        int end = charArray.length-1;
+        boolean isPalindrome = true;
+
+        while (start<end){
+            if (charArray[start] != charArray[end] ){
+                isPalindrome = false;
+            }
+            start++;
+            end--;
+        }
+        return isPalindrome;
+    }
+    public static void main(String[] args) {
+        String value = "madam";
+        System.out.println(isPalindrome2(value));
+
+    }
+}
