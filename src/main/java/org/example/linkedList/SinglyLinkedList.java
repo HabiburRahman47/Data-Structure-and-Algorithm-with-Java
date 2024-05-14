f8dec76003f4ba3d7d10164d8d1f579e89319397
Habibur Rahman <habiburkuetacbd@gmail.com>
2024-05-14 08:23:04 +0600

    print linked list element using display method

--- src/main/java/org/example/linkedList/SinglyLinkedList.java
+++ src/main/java/org/example/linkedList/SinglyLinkedList.java
@@ -10,6 +10,15 @@ public class SinglyLinkedList {
         }
     }
 
+    public void display(){
+        ListNode current = head;
+        while (current != null){
+            System.out.print(current.data+ "-->");
+            current = current.next;
+        }
+        System.out.print("null");
+    }
+
     public static void main(String[] args) {
         SinglyLinkedList sll = new SinglyLinkedList();
         sll.head = new ListNode(10);
@@ -17,20 +26,12 @@ public class SinglyLinkedList {
         ListNode third = new ListNode(8);
         ListNode forth = new ListNode(11);
 
-        System.out.println(sll.head.data);
-        System.out.println(second.data);
-        System.out.println(second.next);
-
         sll.head.next = second;
         second.next = third;
         third.next = forth;
 
-        ListNode current = sll.head;
-        while (current != null){
-            System.out.print(current.data+ "-->");
-            current = current.next;
-        }
-        System.out.print(forth.next);
+        sll.display();
+
 
     }
 }
